2c88c791f1c08a31c5c395167d859b3b5e73b206
Gautam J <gautam.jayapal@gmail.com>
2021-04-20 10:54:31 +0530

    update gd2d add epsilon

--- GradientDescent/gradient_descent_2d.py
+++ GradientDescent/gradient_descent_2d.py
@@ -1,15 +1,12 @@
 import numpy as np
-# import matplotlib.pyplot as plt
-# from matplotlib import animation
-
-# plt.style.use('seaborn')
 
 
 class GradientDescent2D:
 
-    def __init__(self, alpha=1e-2, max_iterations=150, start_point=1.0):
+    def __init__(self, alpha=3e-2, max_iterations=150, start_point=1.0, epsilon=1e-3):
         self.alpha = alpha
         self.maxIterations = max_iterations
+        self.epsilon=epsilon
         self.history = {
             'x': [],
             'grads': [],
@@ -26,6 +23,28 @@ class GradientDescent2D:
         else:
             raise ValueError('start_point must be either "random" or a numerical value')
 
+    def isConverged(self):
+        return abs(self.x - (-1.59791)) <= self.epsilon
+
+    def run(self):
+        # log the starting point of x
+        self.history['x'].append(self.x)
+
+        for i in range(self.maxIterations):
+            gradient = self.grad_f(self.x)
+            self.x = self.x - (self.alpha * gradient)
+
+            # log metrics
+            self.history['x'].append(self.x)
+            self.history['grads'].append(gradient)
+
+            if self.isConverged():
+                print('[INFO] Gradient Descent converged at iteration', i + 1)
+                break
+
+    def getHistory(self):
+        return self.history
+
     @staticmethod
     def f(x):
         # minima at x = -1.59791
@@ -36,58 +55,10 @@ class GradientDescent2D:
         # derivative of the above function
         return 4 * x**3 + 6 * x**2 + 1
 
-    def run(self):
-
-        for _ in range(self.maxIterations):
-            gradient = self.grad_f(self.x)
-            self.x = self.x - (self.alpha * gradient)
-
-            self.history['x'].append(self.x)
-            self.history['grads'].append(gradient)
-
 
 if __name__ == '__main__':
     gd = GradientDescent2D()
     gd.run()
 
-# -------------------------------VISUALIZATION--------------------------
-
-# x = np.linspace(-2.5, 1, 1000)
-# y = f(x)
-
-# fig = plt.figure(figsize=(10, 7))
-# ax1 = fig.add_subplot(111)
-
-# ax1.plot(x, y, c='b', label='function', alpha=0.6)
-# line1, = ax1.plot([], [], label='optimization', c='r', marker='.', alpha=0.4)
-# ax1.set_title(f'Iterations: {max_iters} lr: {alpha}')
-# ax1.set_xlabel('X')
-# ax1.set_ylabel('Y')
-# ax1.legend()
-
-# scatter_x, scatter_y = [], []
-
-
-# def init():
-#     line1.set_data([], [])
-#     return line1,
-
-
-# def animate(i):
-#     x = history['x'][i]
-#     scatter_x.append(x)
-#     scatter_y.append(f(x))
-
-#     line1.set_data(scatter_x, scatter_y)
-#     return line1,
-
-
-# ani = animation.FuncAnimation(fig, animate, init_func=init,
-#                               frames=len(history['x']), blit=True,
-#                               interval=10, repeat=False)
-
-# # saves the animation as .mp4 (takes time, comment if needed)
-# mywriter = animation.FFMpegWriter(fps=30)
-# ani.save(f'Gradient Descent 2D.mp4', writer=mywriter)
-# # show the animation
-# plt.show()
+    print(f'[INFO] Value of x: {gd.x:.5f}')
+    print('[INFO] Expected value: -1.59791')
