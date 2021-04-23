update scripts fix feature scaling

run_linear_regression.py
@@ -44,7 +44,7 @@ def animate(i, dataset, costDataset, line, c_line):
     return line, c_line
 
 
-def plotAndSaveGraphs(lr, args):
+def plotAndSaveGraphs(lr, args, scaler):
     # destructure history object
     history = lr.getHistory()
     thetaHistory = np.array(history['theta'])
@@ -66,9 +66,10 @@ def plotAndSaveGraphs(lr, args):
                     ax=ax1, label='Datapoint')
 
     hypotheses = []
-    fullData = np.linspace(lr.x.min(), lr.x.max(), args.n_samples)
+    fullData = np.linspace(lr.x.min(), lr.x.max(), args.n_samples).reshape(-1, 1)
+    fullData = scaler.transform(fullData)
     fullDataWithOnes = np.concatenate((np.ones((fullData.shape[0], 1)),
-                                       fullData.reshape(-1, 1)), axis=1)
+                                       fullData), axis=1)
 
     for theta in thetaHistory:
         theta = np.array(theta).reshape(-1, 1)
@@ -183,7 +184,7 @@ def main():
     print(f'[DEBUG] Optimized Theta: {optimizedTheta.tolist()}')
     print(f'[DEBUG] Optimized Cost: {optimizedCost}')
 
-    plotAndSaveGraphs(lr, args)
+    plotAndSaveGraphs(lr, args, scaler)
 
 
 if __name__ == "__main__":

run_linear_regression_non_linear.py
@@ -56,7 +56,7 @@ def animate(i, dataset, costDataset, line, c_line):
     return line, c_line
 
 
-def plotAndSaveGraphs(lr, args):
+def plotAndSaveGraphs(lr, args, scaler):
 
     # destructure history object
     history = lr.getHistory()
@@ -73,6 +73,7 @@ def plotAndSaveGraphs(lr, args):
 
     fullData = np.linspace(lr.x[:, 1].min(), lr.x[:, 1].max(), 100).reshape(-1, 1)
     squared = np.concatenate((fullData, (fullData[:, 0]**2).reshape(-1, 1)), axis=1)
+    squared = scaler.transform(squared)
 
     fullDataWithOnes = np.concatenate((np.ones((squared.shape[0], 1)), squared), axis=1)
 
@@ -175,12 +176,11 @@ def main():
     x, y = getCircularDataset(n_samples=args.n_samples,
                               noise=args.noise)
 
-    # FIXME: Add scaler only to x and NOT to x**2
-    scaler = StandardScaler()
-    x = scaler.fit_transform(x)
-
     nf = x[:, 0]**2
     x = np.concatenate((x, nf.reshape(-1, 1)), axis=1)
+
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
 
     lr = LinearRegression(x,
                           y.reshape(-1, 1),
@@ -204,7 +204,7 @@ def main():
     print(f'[DEBUG] Optimized Theta: {optimizedTheta.tolist()}')
     print(f'[DEBUG] Optimized Cost: {optimizedCost}')
 
-    plotAndSaveGraphs(lr, args)
+    plotAndSaveGraphs(lr, args, scaler)
 
 
 if __name__ == "__main__":

run_logistic_regression_non_linear.py
@@ -55,6 +55,7 @@ def plotAndSaveGraphs(lr, args, scaler):
     ys = np.linspace(lr.x[:, 2].min(), lr.x[:, 2].max(), 500)
     X, Y = np.meshgrid(xs, ys)
     temp = np.c_[X.ravel(), Y.ravel(), (X**2).ravel(), (Y**2).ravel()]
+    temp = scaler.transform(temp)
     fullData = np.c_[np.ones((temp.shape[0], 1)), temp]
 
     # make directories
@@ -156,6 +157,22 @@ def plotAndSaveGraphs(lr, args, scaler):
     else:
         plt.show()
 
+    plt.scatter(x1_0, x2_0, label='0', c='k', alpha=0.7)
+    plt.scatter(x1_1, x2_1, label='1', c='g', alpha=0.7)
+    plt.contour(X, Y, lr.getLinearPrediction(fullData, thetaHistory[-1]).reshape(X.shape), [0], cmap='coolwarm')
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.legend()
+    plt.title('Training Data with Decision Boundary')
+
+    if args.save:
+        fileName = os.path.join(pathToDirectory, 'NonLinearTrainingDataWithDecisionBoundary.png')
+        plt.savefig(fileName)
+        print(f'[INFO] Training Data with Decision Boundary Plot saved to {fileName}')
+        plt.close()
+    else:
+        plt.show()
+
 
 def main():
     args = getArguments()
@@ -166,15 +183,15 @@ def main():
                         factor=0.3,
                         random_state=42)
 
-    scaler = StandardScaler()
-    x = scaler.fit_transform(x)
-
     x1Squared = x[:, 0]**2
     x2Squared = x[:, 1]**2
 
     x = np.concatenate((x, x1Squared.reshape(-1, 1)), axis=1)
     x = np.concatenate((x, x2Squared.reshape(-1, 1)), axis=1)
     y = y.reshape(-1, 1)
+
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x)
 
     lr = LogisticRegression(x=x,
                             y=y,