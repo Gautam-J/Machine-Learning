66098b611401794bb7155220962e3390ddc09fcb
Gautam J <gautam.jayapal@gmail.com>
2021-04-23 20:10:21 +0530

    add knn model

---
+++ models/k_nearest_neighbors.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+class KNearestNeighbors:
+
+    def __init__(self, x, y, k=10):
+        self.k = k
+        self.x = x
+        self.y = y
+        print(f'[INFO] Value of k: {self.k}')
+
+        self.initializeModel()
+
+    def initializeModel(self):
+        self.dataset = []
+
+        for i in np.unique(self.y):
+            indices = np.where(self.y == i)[0]
+            class_data = self.x[indices, :]
+            self.dataset.append(class_data)
+
+    def getPrediction(self, newData):
+        self.calculateDistances(newData)
+        votes = self.getVotes()
+        unique = np.unique(votes)
+        # np.unique returns the sorted values
+
+        return unique[0]
+
+    def getVotes(self):
+        votes = [i[1] for i in sorted(self.distances, key=lambda x:x[0])[:self.k]]
+        return votes
+
+    def calculateDistances(self, newData):
+        self.distances = []
+
+        for i, class_ in enumerate(self.dataset):
+            for features in class_:
+                features = np.array(features)
+                # calculate euclidean distance between a single feature point and new_data to be classified
+                euclideanDistance = np.sqrt(np.sum((features - newData)**2))
+                self.distances.append([euclideanDistance, i, features])
